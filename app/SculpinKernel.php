571b736f504eb2e0d8b83a13ed534930555b1ddd
Marcel Führer <mf@move-elevator.de>
2016-09-18 22:21:11 +0200

    add bower components for material design support

---
+++ app/SculpinKernel.php
@@ -0,0 +1,14 @@
+<?php
+
+use Sculpin\Bundle\SculpinBundle\HttpKernel\AbstractKernel;
+
+/**
+ * Class SculpinKernel
+ */
+class SculpinKernel extends AbstractKernel
+{
+    protected function getAdditionalSculpinBundles()
+    {
+        // TODO: Implement getAdditionalSculpinBundles() method.
+    }
+}
