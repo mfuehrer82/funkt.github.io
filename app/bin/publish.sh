1d03999f34992dd8db9da1a2b8d1dba11319b6e0
Marcel Führer <mf@move-elevator.de>
2016-09-20 00:02:52 +0200

    add publish script

--- app/bin/publish.sh
+++ app/bin/publish.sh
@@ -1,4 +1,24 @@
 #!/bin/bash
 
-sculpin generate --env prod
-touch output_prod/.nojekyll
+# make the script fail for any failed command
+set -e
+# make the script display the commands it runs to help debugging failures
+set -x
+
+# Go to the output directory
+cd output_prod
+
+# Remove the existing repo if it exists
+if [ -d ".git" ]; then
+    rm -rf .git
+fi
+
+# Create a repo for the built website for the master branch
+git init
+
+# commit build
+git add .
+git commit -m "Build website"
+
+# push to GitHub Pages
+git push "https://github.com/mfuehrer82/mfuehrer82.github.io.git" -f master
